687fa63b6b9328fe4e3cd62bb4bfda46c84e1db3
Rudy <Rudy.Meijer@gmail.com>
2016-02-09 16:21:59 +0100

    add AssemblyInfo.cs

---
+++ TestTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestTracker")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyDescription(@"
+V101 Add Property Tracker.FinishLine
+V100.5 Add . to load CVB driver.
+V100.4 Add Foundation FBlob.
+V100.3 Add Supervisor can save modifications..
+V100.3 Do not init Calipers on pixelsPerMM change.
+V100.2 Add InitializeStartup.
+V100.1 Updated for CVB 11.2.
+V100 BaseLine.")]
+[assembly: AssemblyConfiguration("Rudy")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTracker")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("d8c585e3-7acc-4da3-883e-f09b311496b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
